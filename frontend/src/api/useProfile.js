20932c9756d9751019c65f604193261e5c9f5296
BitWiseNexus <adityarehpade1@gmail.com>
2025-07-05 13:38:02 +0530

    .evnEXAMPLE added

--- frontend/src/api/useProfile.js
+++ frontend/src/api/useProfile.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/profile";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api/profile";
 
 export const getStudentProfile = async () => {
   const res = await fetch(BASE_URL, {
