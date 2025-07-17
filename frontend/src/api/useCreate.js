6af762e260a8368b5489e812b80d4007059a12f0
amaydixit11 <amaydixit11@gmail.com>
2025-07-17 23:23:20 +0530

    base_url updation

--- frontend/src/api/useCreate.js
+++ frontend/src/api/useCreate.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000/api';
+const BASE_URL = import.meta.env.VITE_BACKEND_URL+"/api" || 'http://localhost:3000/api';
 
 export const createJobPosting = async (jobData, token) => {
   const payload = {
